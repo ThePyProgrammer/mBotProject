f14311d43718de9af0e7b527dcdf7fd95b0207d0
Tran Duc Khang <79127891+KhangTran2005@users.noreply.github.com>
2022-10-29 13:39:19 +0800

    dqsbjhhj

--- code/code.ino
+++ code/code.ino
@@ -9,10 +9,12 @@ MeUltrasonicSensor ultraSensor(PORT_2);
 #define V 150
 #define period 2600
 #define frontSpeed 15.73
-#define dt 6244
+#define T 6244
 
 long start = -1;
 long elapsedTime = 0;
+float previous = 0;
+long n = 0;
 
 void rotation(float angle){
   float t = (period/360) * angle;
@@ -29,9 +31,21 @@ float getDistance(){
   return ultraSensor.distanceCm();
 }
 
-float getUlCorAngle(float prev){
-  float curr = getDistance();
-  
+float getUlCorAngle(float d, float prev, float dt){
+  float dy = d - prev;
+  float ds = frontSpeed * dt * 0.001;
+  return asin(dy/ds);
+}
+
+void moveRobot(float distance){
+    float t = distance / frontSpeed;
+    float v = distance > 0 ? V : -V;
+    t = t > 0 ? t : -t;
+    motor1.run(v);
+    motor2.run(-v);
+    delay(1000 * t);
+    motor1.stop();
+    motor2.stop();
 }
 
 void setup() {
@@ -48,7 +62,7 @@ void loop() {
 //  delay(1000);
     
   //Distance
-//  float d = getDistance();
+  float d = getDistance();
 //  Serial.println(d);
 
 
@@ -57,5 +71,14 @@ void loop() {
  currentTime = micros();
  elapsedTime = start > 0 ? micros() - start : 0;
  start = currentTime;
- Serial.println(elapsedTime);
+// Serial.println(elapsedTime);
+
+ //Correction Angle
+ if (n % 10000){
+  Serial.println(d - previous);
+  previous = d;
+ }
+// Serial.println(getUlCorAngle(d, previous, elapsedTime));
+// moveRobot(10);
+  n++;
 }
--- test/test/test.ino
+++ test/test/test.ino
@@ -9,11 +9,39 @@
 
 
 void setup(){
-  analogWrite(B, Hi);
-  analogWrite(A, Hi);
+//  analogWrite(B, Lo);
+//  analogWrite(A, Hi);
   Serial.begin(9600);
 }
 
 void loop(){
-  Serial.println(analogRead(IR));
+  while(Serial.available() == 0){
+    
+  }
+  String command = Serial.readString();
+  Serial.println(command == "getColor\n");
+  if (command == "getColor\n"){
+    Serial.println("Reading Color");
+    //Green
+    analogWrite(B,Lo);
+    analogWrite(A,Hi);
+    delay(3000);
+    float green = analogRead(LDR);
+    Serial.println(green);
+    //Blue
+    analogWrite(B,Hi);
+    analogWrite(A,Lo);
+    delay(3000);
+    float blue = analogRead(LDR);
+    Serial.println(blue);
+    //Red
+    analogWrite(B,Hi);
+    analogWrite(A,Hi);
+    delay(3000);
+    float red = analogRead(LDR);
+    Serial.println(red);
+    if (green > red && green > blue) Serial.println("Green");
+    if (red > green && red > blue) Serial.println("Red");
+    if (blue > green && blue > red) Serial.println("Blue");
+  }
 }
