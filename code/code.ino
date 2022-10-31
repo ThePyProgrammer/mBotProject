f0fa38eb30b8339103a11bc5303ff02946550c4a
Tran Duc Khang <79127891+KhangTran2005@users.noreply.github.com>
2022-10-31 22:06:33 +0800

    things

--- code/code.ino
+++ code/code.ino
@@ -2,19 +2,31 @@
 
 #include <MeMCore.h>
 
+//Define the Me things
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 MeUltrasonicSensor ultraSensor(PORT_2);
+MeLineFollower line(PORT_1);
 
+//Some constants
 #define V 150
 #define period 2600
 #define frontSpeed 15.73
 #define T 6244
 
+enum robotState{
+  moving,
+  solving,
+  turning,
+  idle
+};
+
+//Global variables
 long start = -1;
 long elapsedTime = 0;
 float previous = 0;
-long n = 0;
+float unit = 1;
+robotState state = idle;
 
 void rotation(float angle){
   float t = (period/360) * angle;
@@ -31,54 +43,42 @@ float getDistance(){
   return ultraSensor.distanceCm();
 }
 
-float getUlCorAngle(float d, float prev, float dt){
-  float dy = d - prev;
-  float ds = frontSpeed * dt * 0.001;
-  return asin(dy/ds);
-}
-
 void moveRobot(float distance){
     float t = distance / frontSpeed;
     float v = distance > 0 ? V : -V;
     t = t > 0 ? t : -t;
-    motor1.run(v);
-    motor2.run(-v);
+    motor1.run(-v);
+    motor2.run(v);
     delay(1000 * t);
     motor1.stop();
     motor2.stop();
 }
 
 void setup() {
-  // put your setup code here, to run once: 
     Serial.begin(9600);
-//  motor1.run(-V);
-//  motor2.run(V);
+    state = moving;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Rotations
-//  rotation(-90);
-//  delay(1000);
-    
-  //Distance
-  float d = getDistance();
-//  Serial.println(d);
-
-
- //Time measure
- long currentTime = 0;
- currentTime = micros();
- elapsedTime = start > 0 ? micros() - start : 0;
- start = currentTime;
-// Serial.println(elapsedTime);
-
- //Correction Angle
- if (n % 10000){
-  Serial.println(d - previous);
-  previous = d;
- }
-// Serial.println(getUlCorAngle(d, previous, elapsedTime));
-// moveRobot(10);
-  n++;
+  int sensorState = line.readSensors();
+  switch(sensorState){
+    case S1_IN_S2_IN: state = idle; break;
+    default: state = moving; break;
+  }
+  switch(state){
+    case moving:
+    moveRobot(unit);
+    break;
+    case solving:
+    break;
+    case turning:
+    break;
+    case idle:
+    break;
+  }
+  //Time measure
+  long currentTime = 0;
+  currentTime = micros();
+  elapsedTime = start > 0 ? micros() - start : 0;
+  start = currentTime;
 }
--- test/test/test.ino
+++ test/test/test.ino
@@ -7,11 +7,89 @@
 #define Hi 255
 #define Lo 0
 
+//Range Scalar for LDR
+float black[3] = {0, 0, 0}; //RGB
+float white[3] = {1, 1, 1}; //RGB
+
+void off(){
+  analogWrite(B, Lo);
+  analogWrite(A, Lo);
+}
 
 void setup(){
-//  analogWrite(B, Lo);
-//  analogWrite(A, Hi);
+  off();
   Serial.begin(9600);
+}
+
+float green(long duration){
+  analogWrite(B,Lo);
+  analogWrite(A,Hi);
+  delay(duration);
+  float ret = analogRead(LDR);
+  off();
+  return ret;
+}
+
+float red(long duration){
+  analogWrite(B,Hi);
+  analogWrite(A,Hi);
+  delay(duration);
+  float ret = analogRead(LDR);
+  off();
+  return ret;
+}
+
+float blue(long duration){
+  analogWrite(B,Hi);
+  analogWrite(A,Lo);
+  delay(duration);
+  float ret = analogRead(LDR);
+  off();
+  return ret;
+}
+
+void getColor(){
+    //Red
+    float r = (red(3000) - black[0])/white[0];
+    Serial.print("Red: ");
+    Serial.println(r);
+    //Green
+    float g = (green(3000) - black[1])/white[1];
+    Serial.print("Green: ");
+    Serial.println(g);
+    //Blue
+    float b = (blue(3000) - black[2])/white[2];
+    Serial.print("Blue: ");
+    Serial.println(b);
+    if (g > r && g > b) Serial.println("Green");
+    if (r > g && r > b) Serial.println("Red");
+    if (b > g && b > r) Serial.println("Blue");
+}
+
+void calibrate(){
+  //Black
+  Serial.println("Show Black");
+  delay(3000);
+  Serial.println("Reading Black");
+  black[0] = red(3000);
+  Serial.println(black[0]);
+  black[1] = green(3000);
+  Serial.println(black[1]);
+  black[2] = blue(3000);
+  Serial.println(black[2]);
+  Serial.println("Done Reading Black");
+
+  //White
+  Serial.println("Show White");
+  delay(3000);
+  Serial.println("Reading White");
+  white[0] = red(3000);
+  Serial.println(white[0]);
+  white[1] = green(3000);
+  Serial.println(white[1]);
+  white[2] = blue(3000);
+  Serial.println(white[2]);
+  Serial.println("Done Reading White");
 }
 
 void loop(){
@@ -19,29 +97,6 @@ void loop(){
     
   }
   String command = Serial.readString();
-  Serial.println(command == "getColor\n");
-  if (command == "getColor\n"){
-    Serial.println("Reading Color");
-    //Green
-    analogWrite(B,Lo);
-    analogWrite(A,Hi);
-    delay(3000);
-    float green = analogRead(LDR);
-    Serial.println(green);
-    //Blue
-    analogWrite(B,Hi);
-    analogWrite(A,Lo);
-    delay(3000);
-    float blue = analogRead(LDR);
-    Serial.println(blue);
-    //Red
-    analogWrite(B,Hi);
-    analogWrite(A,Hi);
-    delay(3000);
-    float red = analogRead(LDR);
-    Serial.println(red);
-    if (green > red && green > blue) Serial.println("Green");
-    if (red > green && red > blue) Serial.println("Red");
-    if (blue > green && blue > red) Serial.println("Blue");
-  }
+  if (command == "calibrate\n") calibrate();
+  else if (command == "getColor\n") getColor();
 }
